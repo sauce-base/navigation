ce77a4eab7f826fb5208fff4ea8b8e5a98c4abf2
roble <renanroble@gmail.com>
2025-12-07 17:42:27 +0000

    Remove Navigation module files and clean up service provider

--- routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Navigation\Http\Controllers\NavigationController;
-
-Route::middleware(['auth:sanctum'])->prefix('api/v1/navigation')->group(function () {
-    Route::apiResource('navigation', NavigationController::class);
-});
--- routes/web.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Navigation\Http\Controllers\NavigationController;
-
-Route::resource('navigation', NavigationController::class);
